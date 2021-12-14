48a1b1a7515bddfa8396d18a6b9347daf3b9588f
刘昌 <1210908391@qq.com>
2021-12-14 18:12:44 +0800

    first commit

---
+++ android/src/main/java/com/liu731/esp_provisioning/EspProvisioningPlugin.java
@@ -0,0 +1,60 @@
+package com.liu731.esp_provisioning;
+
+import androidx.annotation.NonNull;
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+
+/** EspProvisioningPlugin */
+public class EspProvisioningPlugin implements FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private MethodChannel channel;
+
+  @Override
+  public void onAttachedToEngine(@NonNull FlutterPluginBinding flutterPluginBinding) {
+    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), "esp_provisioning");
+    channel.setMethodCallHandler(this);
+  }
+
+  @Override
+  public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+    if (call.method.equals("init")) {
+      byte[] key = call.argument("key");
+      byte[] iv = call.argument("iv");
+
+      IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+      SecretKeySpec secretKeySpec = new SecretKeySpec(key, 0, key.length, "AES");
+      try {
+        this.cipher = Cipher.getInstance("AES/CTR/NoPadding");
+        this.cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+      } catch (NoSuchAlgorithmException e) {
+        e.printStackTrace();
+      } catch (InvalidKeyException e) {
+        e.printStackTrace();
+      } catch (InvalidAlgorithmParameterException e) {
+        e.printStackTrace();
+      } catch (NoSuchPaddingException e) {
+        e.printStackTrace();
+      }
+
+      result.success(true);
+    } else if (call.method.equals("crypt")) {
+      byte[] data = call.argument("data");
+      byte[] ret = cipher.update(data);
+      result.success(ret);
+    } else {
+      result.notImplemented();
+    }
+  }
+
+  @Override
+  public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+    channel.setMethodCallHandler(null);
+  }
+}
---
+++ lib/src/transport_http.dart
@@ -0,0 +1,73 @@
+import 'dart:async';
+import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import 'transport.dart';
+import 'package:string_validator/string_validator.dart';
+
+class TransportHTTP implements Transport {
+  late String _hostname;
+  final Duration _timeout = const Duration(seconds: 10);
+  Map<String, String> _headers = {};
+  final _client = http.Client();
+
+  TransportHTTP(String hostname) {
+    if (!isURL(hostname)) {
+      throw const FormatException('hostname should be an URL.');
+    } else {
+      _hostname = hostname;
+    }
+    _headers["Content-type"] = "application/x-www-form-urlencoded";
+    _headers["Accept"] = "text/plain";
+  }
+
+  @override
+  Future<bool> connect() async {
+    return true;
+  }
+
+  @override
+  Future<void> disconnect() async {
+    _client.close();
+  }
+
+  void _updateCookie(http.Response response) {
+    if (null != response.headers['set-cookie']) {
+      String rawCookie = response.headers['set-cookie']!;
+      int index = rawCookie.indexOf(';');
+      _headers['cookie'] =
+          (index == -1) ? rawCookie : rawCookie.substring(0, index);
+    }
+  }
+
+  @override
+  Future<Uint8List> sendReceive(String epName, Uint8List data) async {
+    try {
+      print("Connecting to " + _hostname + "/" + epName);
+      final response = await _client
+          .post(
+              Uri.http(
+                _hostname,
+                "/" + epName,
+              ),
+              headers: _headers,
+              body: data)
+          .timeout(_timeout)
+          .catchError((error) {
+        print(error);
+      });
+
+      _updateCookie(response);
+      if (response.statusCode == 200) {
+        print('Connection successful');
+        //client.close();
+        final Uint8List bodyBytes = response.bodyBytes;
+        return bodyBytes;
+      } else {
+        print('Connection failed');
+        throw Exception("ESP Device doesn't repond");
+      }
+    } catch (e) {
+      throw StateError('Connection error ' + e.toString());
+    }
+  }
+}
